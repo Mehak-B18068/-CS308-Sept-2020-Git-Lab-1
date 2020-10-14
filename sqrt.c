677eca43ff0b9b93026ea88caeea9b40ede730b6
Mehak Jain <b18068@students.iitmandi.ac.in>
2020-10-15 00:17:56 +0530

    added check to see if the input is a valid number.

--- sqrt.c
+++ sqrt.c
@@ -4,6 +4,8 @@ positive number. */
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
+#include <ctype.h>
 
 //The program starts here
 int main(int argc, char* argv[]) {
@@ -12,8 +14,22 @@ int main(int argc, char* argv[]) {
 		printf("Usage: sqrt input\n\n");
 		exit(-1);
 	}
+	
 	//Input Provided
+	//Check number input
+	char* userInputStr;
+	userInputStr = argv[1];
+    	for ( int x = 0;userInputStr[x]!='\0' ; x++)
+    	{
+        if (!isdigit(userInputStr[x]))
+        {
+            printf("invalid input\n\n");
+		exit(-1);
+        }
+
+    	}
 	int input = atoi(argv[1]);
+	
 	printf("Sqrt of %d is %f\n",input,sqrt(input));
 	printf("End of program. Exiting.\n");
 	return(0);
